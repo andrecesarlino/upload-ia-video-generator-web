2cbf06e41d740937c73ae17f2a4642746c4e9bfa
Andre Lino <andre.cesar97@hotmail.com>
2023-09-14 11:00:58 -0300

    finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,34 @@ import { Label } from "./components/ui/label"
 import { Select, SelectValue, SelectTrigger, SelectContent, SelectItem } from "./components/ui/select"
 import { Slider } from "./components/ui/slider"
 import { VideoInputForm } from "./components/video-input-form"
+import { PromptSelect } from "./components/prompt-select"
+import { useState } from "react"
+import {useCompletion} from 'ai/react'
 
 export function App() {
+
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-type': 'application/json',
+    }
+  })
 
   return (
     
@@ -33,11 +59,14 @@ export function App() {
           <Textarea
           className="resize-none p-4 leading-relaxed"
           placeholder="Inclua o prompt para a IA..." 
+          value={input}
+          onChange={handleInputChange}
           />
           <Textarea
           className="resize-none p-4 leading-relaxed"
           placeholder="Resultado gerado pela IA..."
           readOnly
+          value={completion}
           />
         </div>
         <p className="text-sm text-muted-foreground">
@@ -45,21 +74,15 @@ export function App() {
         </p>
       </div>
       <aside className="w-80 space-y-6">
-        <VideoInputForm />
+
+        <VideoInputForm onVideoUploaded={setVideoId}/>
+
         <Separator />
 
-        <form className="space-y-6">
+        <form onSubmit={handleSubmit} className="space-y-6">
         <div className="space-y-1">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={setInput}/>
               
           </div>
           <div className="space-y-2">
@@ -83,6 +106,8 @@ export function App() {
               min={0}
               max={1}
               step={0.1}
+              value={[temperature]}
+              onValueChange={value => setTemperature(value[0])}
               />
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
                 Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros
@@ -91,7 +116,7 @@ export function App() {
 
           <Separator />
 
-          <Button type="submit" className="w-full">
+          <Button disabled={isLoading} type="submit" className="w-full">
             Executar
             <Wand2 className="w-4 h-4 ml-2" />
           </Button>
--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -6,10 +6,24 @@ import { Button } from "./ui/button";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
-import { log } from "console";
+import { api } from "@/lib/axios";
 
-export function VideoInputForm() {
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!',
+}
+
+interface VideoInputFormProps {
+  onVideoUploaded: (id: string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps) {
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const [status, setStatus] = useState<Status>('waiting')
   const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
@@ -18,6 +32,7 @@ export function VideoInputForm() {
     if(!files) {
       return
     }
+   
 
     const selectedFile = files[0]
 
@@ -66,19 +81,44 @@ export function VideoInputForm() {
   async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
-    const prompt = promptInputRef.current?.value
+    const prompt = promptInputRef.current?.value  
 
     if(!videoFile) {
       return
     }
 
+    setStatus('converting')
+
    
     //converter o video em audio
     const audioFile = await convertVideoToAudio(videoFile)
 
-    console.log(audioFile)
+    //console.log(audioFile, prompt)
+
+    const data = new FormData()
+
+    data.append('file', audioFile)
+
+    setStatus('uploading')
+
+    const response = await api.post('/videos', data)
+
+    const videoId = response.data.video.id
+
+    setStatus('generating')
+
+    await api.post(`/videos/${videoId}/transcription`, {
+      prompt,
+    })
+
+    setStatus('success')
+
+    props.onVideoUploaded(videoId)
+   
 
   }
+
+
 
   const previewURL = useMemo(() => {
     if(!videoFile) {
@@ -111,14 +151,27 @@ export function VideoInputForm() {
           <div className="space-y-2">
             <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
             <Textarea
+            disabled={status !== 'waiting'}
             ref={promptInputRef}
             id="transcription_prompt"
             className="h-20 leading-relaxed resize-none"
             placeholder="Inclua palavras-chave mensionadas no vídeo separadas por virgula (,)"
             />
 
-            <Button type="submit" className="w-full">
-              Carregar Vídeo <Upload className="w-4 h-4 ml-2"/>
+            <Button
+            data-success={status === 'success'}
+            disabled={status !== 'waiting'}
+            type="submit"
+            className="w-full data-[success=true]:bg-emerald-400"
+            >
+              {
+                status === 'waiting' ? (
+                  <>
+                  Carregar Vídeo
+                  <Upload className="w-4 h-4 ml-2"/>
+                  </>
+                ) : statusMessages[status]
+              }
             </Button>
           </div>
         </form>
